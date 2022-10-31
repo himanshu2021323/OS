23ecce69db01fa7134934f3ae24cca18156cc055
himanshu2021323 <108057884+himanshu2021323@users.noreply.github.com>
2022-10-31 15:31:04 +0530

    Update shell.c

--- OS_A1/shell.c
+++ OS_A1/shell.c
@@ -7,123 +7,38 @@
 #include <errno.h>
 #include <sys/wait.h>
 
-char commands[10][10] = {"cd", "echo", "pwd", "exit", "ls", "cat", "date", "rm", "mkdir"};
+char cmds[10][10] = {"cd", "echo", "pwd", "ls", "cat", "date", "rm", "mkdir"};
+int nxt;
 static int count = 0;
-int nextLine;
 
-void cdP(char **input_array)
+void cd(char **array_in)
 {
-    char cwd_inputnew2[1024];
+    int hcs1 = INT_MAX;
+    int hcs2 = INT_MAX;
+    int hcs3 = INT_MAX;
+    int hcs4 = INT_MAX;
+    int hcs5 = INT_MAX;
+    int hcs6 = INT_MAX;
+    int hcs7 = INT_MAX;
 
-    if (input_array[2] != NULL)
+    char cmd_input1[1024];
+
+    if (array_in[1] != NULL)
     {
 
-        strcpy(cwd_inputnew2, input_array[2]);
-        char *pt = strtok(cwd_inputnew2, "\n");
-        char buf[PATH_MAX];
-        char *res = realpath(pt, buf);
-        if (!res)
-        {
-            perror("Error: realpath");
-        }
-        else
-        {
-            int value23 = chdir(res);
-            printf("physical source directory : %s\n", buf);
-            printf("%s\n", res);
-            if (value23 != 0)
-            {
-                perror("Error in command - ");
-            }
-            else
-            {
-                char cwd5[200];
-                char *cwd35 = getcwd(cwd5, sizeof(cwd5));
-            }
-        }
+        strcpy(cmd_input1, array_in[1]);
+        hcs1 = strcmp(cmd_input1, "~");
+        hcs2 = strcmp(cmd_input1, "--");
+        hcs3 = strcmp(cmd_input1, "-");
+        hcs4 = strcmp(cmd_input1, "-P");
+        hcs5 = strcmp(cmd_input1, "-L");
+        hcs6 = strcmp(cmd_input1, "--help");
     }
     else
     {
-        int value2 = chdir(getenv("HOME"));
-        if (value2 != 0)
-        {
-            perror("Error in command - ");
-        }
-        else
-        {
-            char cwd[200];
-            char *cwd3 = getcwd(cwd, sizeof(cwd));
-            printf("%s", cwd3);
-        }
+        hcs7 = 0;
     }
-}
-
-void cdL(char **input_array)
-{
-    if (input_array[2] != NULL)
-    {
-        char cwd_inputnew2[1024];
-        strcpy(cwd_inputnew2, input_array[2]);
-        char *inputnew3 = "";
-        char *inputnew4 = "hi";
-        // printf("yo");
-        char *pt = strtok(cwd_inputnew2, "\n");
-        int id = chdir(pt);
-        if (id != 0)
-        {
-            perror("Error in -L command - ");
-        }
-        else
-        {
-            char cwd[200];
-            char *cwd3 = getcwd(cwd, sizeof(cwd));
-            printf("%s", cwd3);
-        }
-    }
-    else
-    {
-        int value2 = chdir(getenv("HOME"));
-        if (value2 != 0)
-        {
-            perror("Error in command - ");
-        }
-        else
-        {
-            char cwd[200];
-            char *cwd3 = getcwd(cwd, sizeof(cwd));
-            printf("%s", cwd3);
-        }
-    }
-}
-
-void cd(char **input_array)
-{
-    int flaggers1 = INT_MAX;
-    int flaggers2 = INT_MAX;
-    int flaggers3 = INT_MAX;
-    int flaggers4 = INT_MAX;
-    int flaggers5 = INT_MAX;
-    int flaggers6 = INT_MAX;
-    int flaggers7 = INT_MAX;
-
-    char cwd_inputnew1[1024];
-
-    if (input_array[1] != NULL)
-    {
-
-        strcpy(cwd_inputnew1, input_array[1]);
-        flaggers1 = strcmp(cwd_inputnew1, "~");
-        flaggers2 = strcmp(cwd_inputnew1, "--");
-        flaggers3 = strcmp(cwd_inputnew1, "-");
-        flaggers4 = strcmp(cwd_inputnew1, "-P");
-        flaggers5 = strcmp(cwd_inputnew1, "-L");
-        flaggers6 = strcmp(cwd_inputnew1, "--help");
-    }
-    else
-    {
-        flaggers7 = 0;
-    }
-    if (flaggers3 == 0)
+    if (hcs3 == 0)
     {
         int flag3 = chdir("..");
         if (flag3 != 0)
@@ -132,12 +47,12 @@ void cd(char **input_array)
         }
         else
         {
-            char cwd2[200];
-            char *cwd32 = getcwd(cwd2, sizeof(cwd2));
-            printf("%s", cwd32);
+            char cmd2[200];
+            char *cmd32 = getcwd(cmd2, sizeof(cmd2));
+            printf("%s", cmd32);
         }
     }
-    else if (flaggers1 == 0 || flaggers2 == 0 || flaggers7 == 0)
+    else if (hcs1 == 0 || hcs2 == 0 || hcs7 == 0)
     {
         int flag127 = chdir(getenv("HOME"));
         if (flag127 != 0)
@@ -146,27 +61,102 @@ void cd(char **input_array)
         }
         else
         {
-            char cwd[200];
-            char *cwd3 = getcwd(cwd, sizeof(cwd));
-            printf("%s", cwd3);
+            char cmd[200];
+            char *cmd3 = getcwd(cmd, sizeof(cmd));
+            printf("%s", cmd3);
         }
     }
 
-    else if (flaggers6 == 0)
+    else if (hcs6 == 0)
     {
-        printf("%s", "this command is used to change the directory to given input");
+        printf("this command is used to change the directory\n");
     }
-    else if (flaggers4 == 0)
+    else if (hcs4 == 0)
     {
-        cdP(input_array);
+        // cd -P command
+        char cmd_input2[1024];
+        if (array_in[2] != NULL)
+        {
+
+            strcpy(cmd_input2, array_in[2]);
+            char *pt = strtok(cmd_input2, "\n");
+            char buf[PATH_MAX];
+            char *result = realpath(pt, buf);
+            if (!result)
+            {
+                perror("Error: realpath");
+            }
+            else
+            {
+                int value23 = chdir(result);
+                printf("physical source directory : %s\n", buf);
+                printf("%s\n", result);
+                if (value23 != 0)
+                {
+                    perror("Error in command - ");
+                }
+                else
+                {
+                    char cmd5[200];
+                    char *cmd35 = getcwd(cmd5, sizeof(cmd5));
+                }
+            }
+        }
+        else
+        {
+            int value2 = chdir(getenv("HOME"));
+            if (value2 != 0)
+            {
+                perror("Error in command - ");
+            }
+            else
+            {
+                char cmd[200];
+                char *cmd3 = getcwd(cmd, sizeof(cmd));
+                printf("%s", cmd3);
+            }
+        }
     }
-    else if (flaggers5 == 0)
+    else if (hcs5 == 0)
     {
-        cdL(input_array);
+        // cd -L command
+        if (array_in[2] != NULL)
+        {
+            char cmd_input2[1024];
+            strcpy(cmd_input2, array_in[2]);
+            char *inputnew3 = "";
+            char *inputnew4 = "hi";
+            char *pt = strtok(cmd_input2, "\n");
+            int id = chdir(pt);
+            if (id != 0)
+            {
+                perror("Error in -L command - ");
+            }
+            else
+            {
+                char cmd[200];
+                char *cmd3 = getcwd(cmd, sizeof(cmd));
+                printf("%s", cmd3);
+            }
+        }
+        else
+        {
+            int value2 = chdir(getenv("HOME"));
+            if (value2 != 0)
+            {
+                perror("Error in command - ");
+            }
+            else
+            {
+                char cmd[200];
+                char *cmd3 = getcwd(cmd, sizeof(cmd));
+                printf("%s", cmd3);
+            }
+        }
     }
     else
     {
-        char *pt = strtok(cwd_inputnew1, "\n");
+        char *pt = strtok(cmd_input1, "\n");
         int value = chdir(pt);
         if (value != 0)
         {
@@ -174,114 +164,92 @@ void cd(char **input_array)
         }
         else
         {
-            char cwd[200];
-            char *cwd3 = getcwd(cwd, sizeof(cwd));
-            printf("%s", cwd3);
+            char cmd[200];
+            char *cmd3 = getcwd(cmd, sizeof(cmd));
+            printf("%s", cmd3);
         }
     }
 }
 
-void echon(char **input_array, int size)
+void echo(char **array_in, int size)
 {
-    int i = 2;
-    while (i < size)
-    {
-        if (input_array[i] != NULL)
-        {
-            printf("%s ", input_array[i]);
-        }
-        i++;
-    }
-}
-
-void echoE(char **input_array, int size)
-{
-    int i = 2;
-    while (i < size)
-    {
-        if (input_array[i] != NULL)
-        {
-            printf("%s ", input_array[i]);
-        }
-        i++;
-    }
-}
-
-void echo(char **input_array, int size)
-{
-    int flagger4 = INT_MAX;
-    int flagger5 = INT_MAX;
-    int flagger6 = INT_MAX;
+    int hc4 = INT_MAX;
+    int hc5 = INT_MAX;
+    int hc6 = INT_MAX;
     char echo_input[10];
-    if (input_array[1] != NULL)
+    if (array_in[1] != NULL)
     {
-        strcpy(echo_input, input_array[1]);
-        flagger4 = strcmp(echo_input, "-n");
-        flagger5 = strcmp(echo_input, "-E");
-        flagger6 = strcmp(echo_input, "--help");
+        strcpy(echo_input, array_in[1]);
+        hc4 = strcmp(echo_input, "-n");
+        hc5 = strcmp(echo_input, "-E");
+        hc6 = strcmp(echo_input, "--help");
     }
     else
     {
         return;
     }
-    if (flagger4 == 0)
+    if (hc4 == 0)
     {
-        nextLine = 1;
-        echon(input_array, size);
+        // echo -n command
+        nxt = 1;
+        int i = 2;
+        while (i < size)
+        {
+            if (array_in[i] != NULL)
+            {
+                printf("%s ", array_in[i]);
+            }
+            i++;
+        }
     }
-    else if (flagger5 == 0)
+    else if (hc5 == 0)
     {
-        echoE(input_array, size);
+        // echo -E command
+        int i = 2;
+        while (i < size)
+        {
+            if (array_in[i] != NULL)
+            {
+                printf("%s ", array_in[i]);
+            }
+            i++;
+        }
     }
-    else if (flagger6 == 0)
+    else if (hc6 == 0)
     {
-        printf("%s", "this command prints the input given after echo");
+        printf("this command prints the input given after echo");
     }
     else
     {
-        for (int i = 1; i < size; ++i)
+        int i = 1;
+        while (i < size)
         {
-            if (input_array[i] != NULL)
+            if (array_in[i] != NULL)
             {
-                printf("%s ", input_array[i]);
+                printf("%s ", array_in[i]);
             }
+            ++i;
         }
     }
 }
 
-void pwdP()
-{
-    char newarr[1024];
-    char *cwd2 = getcwd(newarr, sizeof(newarr));
-    printf("%s", cwd2);
-}
-
-void pwdL()
-{
-    char buf[PATH_MAX];
-    char newarr[1024];
-    char *res = realpath(newarr, buf);
-    char *cwd2 = getcwd(newarr, sizeof(newarr));
-    printf("%s", cwd2);
-}
-
-void pwd(char **input_array)
+void pwd(char **array_in)
 {
     int newflag = 0;
-    char cwd_input[10];
+    char cmd_input[10];
     char arr[1024];
-    int flagger2 = INT_MAX;
-    int flagger3 = INT_MAX;
-    int flagger4 = INT_MAX;
+    int hc2 = INT_MAX;
+    int hc3 = INT_MAX;
+    int hc4 = INT_MAX;
 
-    if (input_array[1] != NULL)
+    if (array_in[1] != NULL)
     {
-        strcpy(cwd_input, input_array[1]);
+        strcpy(cmd_input, array_in[1]);
         newflag = 1;
     }
 
-    char *cwd = getcwd(arr, sizeof(arr));
-    if (cwd == NULL)
+    char *cmd = getcwd(arr, sizeof(arr));
+    if (cmd == NULL)
     {
         perror("Error:");
     }
@@ -289,41 +257,50 @@ void pwd(char **input_array)
     {
         if (newflag == 1)
         {
-            flagger2 = strcmp(cwd_input, "-P");
-            flagger3 = strcmp(cwd_input, "--help");
-            flagger4 = strcmp(cwd_input, "-L");
-            if (flagger3 == 0)
+            hc2 = strcmp(cmd_input, "-P");
+            hc3 = strcmp(cmd_input, "--help");
+            hc4 = strcmp(cmd_input, "-L");
+            if (hc3 == 0)
             {
                 printf("%s", "this command prints the current working directory");
                 exit;
             }
-            else if (flagger2 == 0)
+            else if (hc2 == 0)
             {
-                pwdP();
+                // pwd -P command
+                    char newarr[1024];
+                    char *cmd2 = getcwd(newarr, sizeof(newarr));
+                    printf("%s", cmd2);
             }
-            else if (flagger4 == 0)
+            else if (hc4 == 0)
             {
-                pwdL();
+                // pwd -L command
+                char buf[PATH_MAX];
+                char newarr[1024];
+                char *result = realpath(newarr, buf);
+                char *cmd2 = getcwd(newarr, sizeof(newarr));
+                printf("%s", cmd2);
             }
         }
         else
         {
-            printf("%s", cwd);
+            printf("%s", cmd);
         }
     }
 }
 
 int main()
 {
-    printf("\e[1;1H\e[2J");
-    printf("\n\n\n\n*******************************Welcome To My SHELL***********************************\n\n\n\n");
+    printf("****************************************************************************\n\n\n\n");
+    printf("\t\t\t Welcome To My Shell");
+    printf("****************************************************************************\n\n\n\n");
     printf("Available commands:\n\tcd, pwd, history, echo, ls, cat, date, mkdir, rm, exit\n\n");
-    char cwd[100];
+    char cmd[100];
     while (1)
     {
-        nextLine = 0;
+        nxt = 0;
         char command[1024];
-        printf("$ ");
+        printf("[himanshu@shell ~]$ ");
 
         scanf("%[^\n]%*c", command);
 
@@ -331,77 +308,75 @@ int main()
         {
             int size = 0;
             char func[9];
-            int flagger = -1;
-            int func_no = -1;
+            int hc = -1;
+            int function = -1;
             char new_command[1024];
-            char **input_array = (char **)malloc(500 * sizeof(char *));
+            char **array_in = (char **)malloc(500 * sizeof(char *));
 
             strcpy(new_command, command);
             if (count > 999)
             {
                 count = 0;
             }
-            // strcpy(history_array[count++], new_command);
+            char *tkn = strtok(command, " ");
 
-            char *token = strtok(command, " ");
-
-            while (token != NULL)
+            while (tkn)
             {
-                input_array[size] = (char *)malloc(500 * sizeof(char));
-                strcpy(input_array[size], token);
-                token = strtok(NULL, " ");
+                array_in[size] = (char *)malloc(500 * sizeof(char));
+                strcpy(array_in[size], tkn);
+                tkn = strtok(NULL, " ");
                 size++;
             }
 
             int i = 0;
             while (i < 10)
             {
-                int ctr = 0;
+                int control = 0;
 
                 int flag = 0;
 
-                while (commands[i][ctr] != '\0' && input_array[0][ctr] != '\0')
+                while (cmds[i][control] != '\0' && array_in[0][control] != '\0')
                 {
-                    if (commands[i][ctr] != input_array[0][ctr])
+                    if (cmds[i][control] != array_in[0][control])
                     {
                         flag = 1;
                         break;
                     }
-                    ctr++;
+                    control++;
                 }
 
-                if (commands[i][ctr] != '\0' || input_array[0][ctr] != '\0')
+                if (cmds[i][control] != '\0' || array_in[0][control] != '\0')
                 {
                     flag = 1;
                 }
 
                 if (flag != 1)
                 {
-                    func_no = i;
+                    function = i;
                     break;
                 }
                 i++;
             }
 
-            if (func_no == 0)
+            if (function == 0)
             {
-                cd(input_array);
+                cd(array_in);
             }
-            else if (func_no == 1)
+            else if (function == 1)
             {
-                echo(input_array, size);
+                echo(array_in, size);
             }
-            else if (func_no == 2)
+            else if (function == 2)
             {
-                pwd(input_array);
+                pwd(array_in);
             }
-            else if (func_no == 3)
+            else if (function == 3)
             {
                 printf("[PROCESS COMPLETED]\n");
                 break;
             }
 
-            else if (func_no == 4)
+            else if (function == 4)
             {
                 pid_t id;
                 int stat;
@@ -417,7 +392,7 @@ int main()
                     time = wait(&stat);
                 }
             }
-            else if (func_no == 5)
+            else if (function == 5)
             {
                 pid_t id;
                 int stat;
@@ -433,7 +408,7 @@ int main()
                     time = wait(&stat);
                 }
             }
-            else if (func_no == 6)
+            else if (function == 6)
             {
                 pid_t id;
 
@@ -450,7 +425,7 @@ int main()
                     time = wait(&stat);
                 }
             }
-            else if (func_no == 7)
+            else if (function == 7)
             {
                 pid_t id;
                 int stat;
@@ -466,7 +441,7 @@ int main()
                     time = wait(&stat);
                 }
             }
-            else if (func_no == 8)
+            else if (function == 8)
             {
                 pid_t id;
                 int stat;
@@ -485,16 +460,16 @@ int main()
             }
             else
             {
-                printf("%s", "Error: Invalid Command");
+                printf("Error: Invalid Command\n");
             }
-            if (nextLine == 0)
+            if (nxt == 0)
             {
-                printf("%s\n", "");
+                printf(" \n");
             }
         }
         else
         {
-            printf("%s\n", "PLEASE RUN AGAIN, THERE WAS NO INPUT");
+            printf("THERE IS NO INPUT FOUND, RUN AGAIN PLEASE\n");
         }
     }
     return 0;
