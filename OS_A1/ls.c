110d12061ef05e5b24acac1e9f6d25a275fba2bd
himanshu2021323 <108057884+himanshu2021323@users.noreply.github.com>
2022-10-31 19:31:50 +0530

    Update ls.c

--- OS_A1/ls.c
+++ OS_A1/ls.c
@@ -10,27 +10,28 @@
 #include <grp.h>
 
 
-void LsL(char argument[1000])
+void LsL(char arg[1000])
 {
-    DIR *thedirectory;
-    struct dirent *thefile;
+    DIR *directory;
+    struct dirent *file;
     struct stat thestat;
-    struct passwd *tf;
-    struct group *gf;
-    char buf[512];
-    if (strlen(argument) == 0)
+    struct passwd *password;
+    struct group *groups;
+    char arr[512];
+    if (strlen(arg) == 0)
     {
-        argument = ".";
+        arg = ".";
     }
-    thedirectory = opendir(argument);
-    while ((thefile = readdir(thedirectory)) != NULL)
+    directory = opendir(arg);
+    file = readdir(directory);
+    while (file)
     {
-        if (thefile->d_name[0] == '.')
+        if (file->d_name[0] == '.')
         {
             continue;
         }
-        sprintf(buf, "%s/%s", argument, thefile->d_name);
-        stat(buf, &thestat);
+        sprintf(arr, "%s/%s", arg, file->d_name);
+        stat(arr, &thestat);
         switch (thestat.st_mode & S_IFMT)
         {
         case S_IFBLK:
@@ -65,41 +66,41 @@ void LsL(char argument[1000])
         printf((thestat.st_mode & S_IWOTH) ? "w" : "-");
         printf((thestat.st_mode & S_IXOTH) ? "x" : "-");
         printf(" %ld", thestat.st_nlink);
-        tf = getpwuid(thestat.st_uid);
-        printf(" %s", tf->pw_name);
-        gf = getgrgid(thestat.st_gid);
-        printf(" %s", gf->gr_name);
+        password = getpwuid(thestat.st_uid);
+        printf(" %s", password->pw_name);
+        groups = getgrgid(thestat.st_gid);
+        printf(" %s", groups->gr_name);
         printf(" %zu", thestat.st_size);
-        printf(" %s", thefile->d_name);
+        printf(" %s", file->d_name);
         printf(" %s", ctime(&thestat.st_mtime));
     }
-    closedir(thedirectory);
+    closedir(directory);
 }
 
 int main(int argc, char *argv[])
 {
-    char commandName[10] = "";
+    char cmd[10] = "";
     char flag[10] = "";
-    char argument[1000] = "";
+    char arg[1000] = "";
     if (argc > 1)
     {
-        char *token = strtok(argv[1], " ");
-        strcpy(commandName, token);
-        token = strtok(NULL, " ");
-        if (token != NULL)
+        char *tkn = strtok(argv[1], " ");
+        strcpy(cmd, tkn);
+        tkn = strtok(NULL, " ");
+        if (tkn != NULL)
         {
-            if (token[0] == '-')
+            if (tkn[0] == '-')
             {
-                strcpy(flag, token);
-                token = strtok(NULL, " ");
-                if (token != NULL)
+                strcpy(flag, tkn);
+                tkn = strtok(NULL, " ");
+                if (tkn != NULL)
                 {
-                    strcpy(argument, token);
+                    strcpy(arg, tkn);
                 }
             }
             else
             {
-                strcpy(argument, token);
+                strcpy(arg, tkn);
             }
         }
     }
@@ -107,18 +108,18 @@ int main(int argc, char *argv[])
     {
         struct dirent **names;
         int n;
-        if (strlen(argument) == 0)
+        if (strlen(arg) == 0)
         {
             n = scandir(".", &names, NULL, alphasort);
         }
         else
         {
-            n = scandir(argument, &names, NULL, alphasort);
+            n = scandir(arg, &names, NULL, alphasort);
         }
         int i = 0;
         if (n < 0)
         {
-            perror("scandir");
+            perror("Error: scandir");
         }
         else
         {
@@ -140,18 +141,18 @@ int main(int argc, char *argv[])
     {
         struct dirent **names;
         int n;
-        if (strlen(argument) == 0)
+        if (strlen(arg) == 0)
         {
             n = scandir(".", &names, NULL, alphasort);
         }
         else
         {
-            n = scandir(argument, &names, NULL, alphasort);
+            n = scandir(arg, &names, NULL, alphasort);
         }
         int i = 0;
         if (n < 0)
         {
-            perror("scandir");
+            perror("Error: scandir");
         }
         else
         {
@@ -166,11 +167,11 @@ int main(int argc, char *argv[])
     }
     else if (flag[1] == 'l')
     {
-        printLsL(argument);
+        LsL(arg);
     }
     else
     {
-        printf("Invalid Input -- %s\n", flag);
+        printf("Invalid Input ---> %s\n", flag);
         return 1;
     }
     return 0;
