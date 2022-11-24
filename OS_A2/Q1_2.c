75ed29cfb11b6fef3c8b7bbd49e583d56b490e68
himanshu2021323 <108057884+himanshu2021323@users.noreply.github.com>
2022-11-25 02:12:42 +0530

    Update Q1_2.c

--- OS_A2/Q1_2.c
+++ OS_A2/Q1_2.c
@@ -13,39 +13,39 @@ int main()
     pid1 = fork();
     if (pid1 == 0)
     {
-        clock_gettime(CLOCK_REALTIME, &finish);
-        double time = finish.tv_sec - start.tv_sec;
-        time += (finish.tv_nsec - start.tv_nsec) / 1000000000.0;
-        printf("Time taken to compile the Linux kernel: %lf seconds\n", time);
         execl("/bin/sh", "sh", "script1.sh", (char *)NULL);
     }
     else {
         int status;
         waitpid(pid1, &status, 0);
+        clock_gettime(CLOCK_REALTIME, &finish);
+        double time = finish.tv_sec - start.tv_sec;
+        time += (finish.tv_nsec - start.tv_nsec) / 1000000000.0;
+        printf("Time taken to compile the Linux kernel: %lf seconds\n", time);
         pid2 = fork();
         if(pid2 == 1)
         {
             execl("/bin/sh", "sh", "script2.sh", (char *)NULL);
-            clock_gettime(CLOCK_REALTIME, &finish);
-            double time = finish.tv_sec - start.tv_sec;
-            time += (finish.tv_nsec - start.tv_nsec) / 1000000000.0;
-            printf("Time taken to compile the Linux kernel: %lf seconds\n", time);
         }
         else{
             int status2;
             waitpid(pid2, &status2, 0);
-            pid3 = fork();
-            if (pid3 == 2)
-            {
-            execl("/bin/sh", "sh", "script3.sh", (char *)NULL);
             clock_gettime(CLOCK_REALTIME, &finish);
             double time = finish.tv_sec - start.tv_sec;
             time += (finish.tv_nsec - start.tv_nsec) / 1000000000.0;
             printf("Time taken to compile the Linux kernel: %lf seconds\n", time);
+            pid3 = fork();
+            if (pid3 == 2)
+            {
+                execl("/bin/sh", "sh", "script3.sh", (char *)NULL);
             }
             else{
                 int status3;
                 waitpid(pid3, &status3, 0);
+                clock_gettime(CLOCK_REALTIME, &finish);
+                double time = finish.tv_sec - start.tv_sec;
+                time += (finish.tv_nsec - start.tv_nsec) / 1000000000.0;
+                printf("Time taken to compile the Linux kernel: %lf seconds\n", time);
 
             }
         }
