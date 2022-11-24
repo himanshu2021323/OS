33c0b3bafffa62bf72f3402c241543d3e39c9f9b
himanshu2021323 <108057884+himanshu2021323@users.noreply.github.com>
2022-11-25 02:02:00 +0530

    Update Q1_2.c

--- OS_A2/Q1_2.c
+++ OS_A2/Q1_2.c
@@ -10,18 +10,20 @@ int main()
     int i, j;
     struct timespec start, finish;
     clock_gettime(CLOCK_REALTIME, &start);
-    pid = fork();
-    if (pid == 0)
+    pid1 = fork();
+    if (pid1 == 0)
     {
-        execl("/bin/sh", "sh", "script1.sh", (char *)NULL);
         clock_gettime(CLOCK_REALTIME, &finish);
         double time = finish.tv_sec - start.tv_sec;
         time += (finish.tv_nsec - start.tv_nsec) / 1000000000.0;
         printf("Time taken to compile the Linux kernel: %lf seconds\n", time);
+        execl("/bin/sh", "sh", "script1.sh", (char *)NULL);
     }
     else {
-        pid1 = fork();
-        if(pid1 == 1)
+        int status;
+        waitpid(pid1, &status, 0);
+        pid2 = fork();
+        if(pid2 == 1)
         {
             execl("/bin/sh", "sh", "script2.sh", (char *)NULL);
             clock_gettime(CLOCK_REALTIME, &finish);
@@ -30,14 +32,21 @@ int main()
             printf("Time taken to compile the Linux kernel: %lf seconds\n", time);
         }
         else{
-            pid2 = fork();
-            if (pid2 == 2)
+            int status2;
+            waitpid(pid2, &status2, 0);
+            pid3 = fork();
+            if (pid3 == 2)
             {
             execl("/bin/sh", "sh", "script3.sh", (char *)NULL);
             clock_gettime(CLOCK_REALTIME, &finish);
             double time = finish.tv_sec - start.tv_sec;
             time += (finish.tv_nsec - start.tv_nsec) / 1000000000.0;
             printf("Time taken to compile the Linux kernel: %lf seconds\n", time);
+            }
+            else{
+                int status3;
+                waitpid(pid3, &status3, 0);
+
             }
         }
     }
