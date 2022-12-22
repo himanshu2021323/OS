8a355216ac6b617ebddbea40f95430ae0a3059de
himanshu2021323 <108057884+himanshu2021323@users.noreply.github.com>
2022-12-22 12:46:57 +0530

    Update helpers.c

--- OS_A3/Mine/Queue/helpers.c
+++ OS_A3/Mine/Queue/helpers.c
@@ -5,65 +5,67 @@
 #include <stdlib.h>
 #include <unistd.h>
 
-static char *generateRandomString(size_t length){
+static char *getRS(size_t len){
     char Choices[] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
-    char *randomlyGeneratedString = malloc(30);
-    for (int i = 0; i < length; i++){
-        randomlyGeneratedString[i] = Choices[rand() % (int) (sizeof Choices - 1)];
+    char *generatedString = malloc(30);
+    for (int i = 0; i < len; i++){
+        generatedString[i] = Choices[rand() % (int) (sizeof Choices - 1)];
     }
-    return randomlyGeneratedString;
+
+    return generatedString;
 }
 
-static char *addIndexasTrailtoString(char *randomlyGeneratedString, size_t length, int stringIndex){
+static char *indextoString(char *generatedString, size_t len, int stringIndex){
 
-    int tensPlace = ((stringIndex/10)%10);
-    int onesPlace = (stringIndex%10);
-    randomlyGeneratedString[length] = ' ';
-    randomlyGeneratedString[length+1] = tensPlace + '0';
-    randomlyGeneratedString[length+2] = onesPlace + '0';
-    randomlyGeneratedString[length+3] = '\n';
-    return randomlyGeneratedString;
+    int tens = ((stringIndex/10)%10);
+    int ones = (stringIndex%10);
+    generatedString[len] = ' ';
+    generatedString[len+1] = tens + '0';
+    generatedString[len+2] = ones + '0';
+    generatedString[len+3] = '\n';
+    return generatedString;
 }
 
-static char *getRandomString(char *randomlyGeneratedString, size_t length,int stringIndex){
-    randomlyGeneratedString = generateRandomString(length);
-    randomlyGeneratedString = addIndexasTrailtoString(randomlyGeneratedString, length, stringIndex);
-    // printf("%s", randomlyGeneratedString);
-    // exit(0);
-    return randomlyGeneratedString;
+static char *getRandomString(char *generatedString, size_t len,int stringIndex){
+    generatedString = getRS(len);
+    generatedString = indextoString(generatedString, len, stringIndex);
+
+    return generatedString;
 }
 
-static void endConnection(register int s){
+static void end(register int s){
     close(s);
 }
 
-static FILE *readfromSocket(register int connectionToClient){
-    FILE *filePointer;
-    filePointer = fdopen(connectionToClient, "r");
+static FILE *socketRead(register int conection){
+    FILE *fp;
+    fp = fdopen(conection, "r");
 }
 
-static void throwPerrorandExit(char *str){
+static void errorandexit(char *str){
     perror(str);
     exit(1);
 }
 
-static void populateArraywithRandomStrings(char* arrayToPopulate[], int lenOfArray){
+static void arrayRS(char* popilateArray[], int lenOfArray){
     for(int i = 0; i < lenOfArray; i++){
-        char *randomlyGeneratedString = malloc(20);
-        arrayToPopulate[i] = getRandomString(randomlyGeneratedString, 20, i);
+        char *generatedString = malloc(20);
+        popilateArray[i] = getRandomString(generatedString, 20, i);
     }
 }
 
-static void SendandReadSignals(register int connectionToClient, char *listOfRandomlyGeneratedString[50]){
+static void sendAndread(register int conection, char *listOfRS[50]){
     int stringID=0;
+
     while(stringID<50){
-        for (int i = stringID; i < stringID+5; i++)
-        send(connectionToClient, listOfRandomlyGeneratedString[i], strlen(listOfRandomlyGeneratedString[i]), 0);
-        char number[100];
+        for (int i = 0; i < 5; i++)
+        send(conection, listOfRS[i], strlen(listOfRS[i]), 0);
+
+        char num[100];
         int i=0;
-        int valread = read(connectionToClient,number, 100);
-        printf("Number Received - %s\n",number);
-        stringID = atoi(number);
+        int valread = read(conection,num, 100);
+        printf("Number Received - %s\n",num);
+        stringID = atoi(num);
     }
 }
 
