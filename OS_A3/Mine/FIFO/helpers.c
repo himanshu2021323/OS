7c44432acb023b631c243568b4283f269f39a08e
himanshu2021323 <108057884+himanshu2021323@users.noreply.github.com>
2022-12-22 12:45:09 +0530

    Update helpers.c

--- OS_A3/Mine/FIFO/helpers.c
+++ OS_A3/Mine/FIFO/helpers.c
@@ -1,75 +1,83 @@
-#include <sys/types.h>
-#include <sys/socket.h>
-#include <sys/un.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <sys/un.h>
 
-static char *generateRandomString(size_t length){
+static char *getRS(size_t len){
     char Choices[] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
-    char *randomlyGeneratedString = malloc(30);
-    for (int i = 0; i < length; i++){
-        randomlyGeneratedString[i] = Choices[rand() % (int) (sizeof Choices - 1)];
+    char *generatedString = malloc(30);
+
+    for (int i = 0; i < len; i++){
+        generatedString[i] = Choices[rand() % (int) (sizeof Choices - 1)];
     }
-    return randomlyGeneratedString;
+    
+    return generatedString;
 }
 
-static char *addIndexasTrailtoString(char *randomlyGeneratedString, size_t length, int stringIndex){
-    int tensPlace = ((stringIndex/10)%10);
-    int onesPlace = (stringIndex%10);
-    int lengthOfTensPlace = snprintf( NULL, 0, "%d", tensPlace);
-    char* tensPlaceAsString = malloc( lengthOfTensPlace + 1 );
-    snprintf( tensPlaceAsString, lengthOfTensPlace + 1, "%d", tensPlace );
-    int lengthOfOnesPlace = snprintf( NULL, 0, "%d", onesPlace);
-    char* onesPlaceAsString = malloc( lengthOfOnesPlace + 1 );
-    snprintf( onesPlaceAsString, lengthOfOnesPlace + 1, "%d", tensPlace );
-    randomlyGeneratedString[length+1] = tensPlace + '0';
-    randomlyGeneratedString[length+2] = onesPlace + '0';
-    randomlyGeneratedString[length+3] = '\n';
-    return randomlyGeneratedString;
+static char *indextoString(char *generatedString, size_t len, int stringIndex){
+    int tens = ((stringIndex/10)%10);
+    int lengthOftens = snprintf( NULL, 0, "%d", tens);
+    char* tensAsString = malloc( lengthOftens + 1 );
+    snprintf(tensAsString, lengthOftens + 1, "%d", tens );
+
+    int ones = (stringIndex%10);
+    int lengthOfones = snprintf( NULL, 0, "%d", ones);
+    char* onesAsString = malloc( lengthOfones + 1 );
+    snprintf( onesAsString, lengthOfones + 1, "%d", tens );
+
+    generatedString[len+1] = tens + '0';
+    generatedString[len+2] = ones + '0';
+    generatedString[len+3] = '\n';
+
+    return generatedString;
 }
 
-static char *getRandomString(char *randomlyGeneratedString, size_t length,int stringIndex){
-    randomlyGeneratedString = generateRandomString(length);
-    randomlyGeneratedString = addIndexasTrailtoString(randomlyGeneratedString, length, stringIndex);
-    return randomlyGeneratedString;
+static char *getRandomString(char *generatedString, size_t len,int stringIndex){
+    generatedString = getRS(len);
+    generatedString = indextoString(generatedString, len, stringIndex);
+
+    return generatedString;
 }
 
-static void endConnection(register int s){
+static void end(register int s){
     close(s);
 }
 
-static FILE *readfromSocket(register int connectionToClient){
-    FILE *filePointer;
-    filePointer = fdopen(connectionToClient, "r");
+static FILE *socketRead(register int connection){
+    FILE *fp;
+    fp = fdopen(connection, "r");
 }
 
-static void throwPerrorandExit(char *str){
+static void errorandexit(char *str){
     perror(str);
     exit(1);
 }
 
-static void populateArraywithRandomStrings(char* arrayToPopulate[], int lenOfArray){
+static void arrayRS(char* populateArray[], int lenOfArray){
     for(int i = 0; i < lenOfArray; i++){
-        char *randomlyGeneratedString = malloc(20);
-        arrayToPopulate[i] = getRandomString(randomlyGeneratedString, 20, i);
+        char *generatedString = malloc(20);
+        populateArray[i] = getRandomString(generatedString, 20, i);
     }
 }
 
-static void SendandReadSignals(register int connectionToClient, char *listOfRandomlyGeneratedString[50]){
+static void sendAndread(register int connection, char *listOfRS[50]){
     int stringID=0;
+
     while(stringID<50){
-        for (int i = stringID; i < stringID+5; i++)
-        send(connectionToClient, listOfRandomlyGeneratedString[i], strlen(listOfRandomlyGeneratedString[i]), 0);
-        char number[100];
+        for (int i = 0; i < 5; i++)
+        send(connection, listOfRS[i], strlen(listOfRS[i]), 0);
+
+        char num[100];
         int i=0;
-        int valread = read(connectionToClient,number, 100);
-        printf("Number Received - %s\n",number);
-        stringID = atoi(number);
+        int valread = read(connection,num, 100);
+        printf("Number Received - %s\n",num);
+        stringID = atoi(num);
     }
 }
 
 static void printString(int i, char *str1){
-    printf("Sent to Server: %d\n",i);
+    printf("Server Sent to: %d\n",i);
     printf("%s", (str1));
 }
